b7fe9bc9ec9ca1aab45f0a3859b47b93ac954369
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-07-02 18:50:37 +0530

    minor fixes before deployement

--- src/Checkout.js
+++ src/Checkout.js
@@ -15,8 +15,9 @@ export const Checkout = () => {
                     className='checkout_ad'
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
 
-                <div className="checkout_title">
-                    <h2>Your shopping basket</h2>
+                <div>
+                    <h3>Hello {user?.email}</h3>
+                    <h2 className="checkout_title" >Your shopping basket</h2>
 
                     {basket.map(item => (
                         <CheckoutProduct
