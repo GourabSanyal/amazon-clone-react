83904685d426ff74886db9ae030bad09d03c46d3
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-07-10 22:30:37 +0530

    Functions due

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require("firebase-functions");
+const express = require('express');
+const cors = require('cors');
+const stripe = require('AIzaSyAC_zCFjKf1Boj6T8ELf_FOxXQYxFPE5kE')
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
