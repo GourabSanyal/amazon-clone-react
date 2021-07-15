04a4f46a30a173b1b82f708c34f8412f5b3ec187
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-07-15 22:41:17 +0530

    Minor axios bug fixed, orders.js added

--- src/axios.js
+++ src/axios.js
@@ -4,4 +4,4 @@ const instance = axios.create({
     baseURL: 'http://localhost:5001/clone-baadf/us-central1/api' //The API end point (cloud function) URL
 });
 
-export default instance;
+export default instance ;
