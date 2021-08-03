d4775d87d14c8b1f4f4b8e1ba16f707b25e7e01d
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-08-03 19:02:21 +0530

    Order Page added

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import { db } from './firebase';
 import './Orders.css' 
+import { useStateValue } from './StateProvider'
 
 function Orders() {
+    const [{basket, user}, dispatch ] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        if(user) {
+            
+            db
+            .collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .onSnapshot(snapshot.doc.map(doc =>({
+                id: doc.id,
+                data: doc.data()
+            })))
+        } else { 
+            setOrders([])
+        }
+    }, [user])
+
     return (
         <div className='orders'>
-            <h1>Your Order</h1>
+            <h1>Your Orders</h1>
+
+            <div className="orders_order">
+                {orders?.map(order =>(
+                    <Order order={order} />
+                ))}
+            </div>
+
         </div>
     )
 }
